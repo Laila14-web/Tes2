1dd6ca30ac870ad0f90704cc3988f0e10d145762
Laila14-web <lailahabiba1403@gmail.com>
2025-02-08 21:42:37 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,141 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5la1ucIxy41":
+        Script1();
+        break;
+      case "64eb4Q7ilEm":
+        Script2();
+        break;
+      case "6SZrIspFTrT":
+        Script3();
+        break;
+      case "60UJIkaD3sP":
+        Script4();
+        break;
+      case "6deqDwLqZiK":
+        Script5();
+        break;
+      case "5yplJ04wY2v":
+        Script6();
+        break;
+      case "5V69KRd3LsV":
+        Script7();
+        break;
+      case "6EndzjIou8Q":
+        Script8();
+        break;
+      case "6g04MPTsqnm":
+        Script9();
+        break;
+      case "6JRpkDsIGPI":
+        Script10();
+        break;
+      case "5z22XHeFEzK":
+        Script11();
+        break;
+      case "6bWn5otx2zl":
+        Script12();
+        break;
+      case "6oHOwTmkbl9":
+        Script13();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src="backsound.mp3";
+
+audio.load();
+
+audio.play();
+
+}
+
+function Script2()
+{
+  var audio = document.getElementByld("bgSongku");
+
+audio.volume = 0,0;
+}
+
+function Script3()
+{
+  var audio = document.getElementByld("bgSongku");
+
+audio.volume = 0,1;
+}
+
+function Script4()
+{
+  var audio = document.getElementByld("bgSongku");
+
+audio.volume = 0,2;
+}
+
+function Script5()
+{
+  var audio = document.getElementByld("bgSongku");
+
+audio.volume = 0,3;
+}
+
+function Script6()
+{
+  var audio = document.getElementByld("bgSongku");
+
+audio.volume = 0,4;
+}
+
+function Script7()
+{
+  var audio = document.getElementByld("bgSongku");
+
+audio.volume = 0,5;
+}
+
+function Script8()
+{
+  var audio = document.getElementByld("bgSongku");
+
+audio.volume = 0,0;
+}
+
+function Script9()
+{
+  var audio = document.getElementByld("bgSongku");
+
+audio.volume = 0,1;
+}
+
+function Script10()
+{
+  var audio = document.getElementByld("bgSongku");
+
+audio.volume = 0,2;
+}
+
+function Script11()
+{
+  var audio = document.getElementByld("bgSongku");
+
+audio.volume = 0,3;
+}
+
+function Script12()
+{
+  var audio = document.getElementByld("bgSongku");
+
+audio.volume = 0,4;
+}
+
+function Script13()
+{
+  var audio = document.getElementByld("bgSongku");
+
+audio.volume = 0,5;
+}
+
